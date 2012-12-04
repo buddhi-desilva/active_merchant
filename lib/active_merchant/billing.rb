88d0a3ad786849a0c5d66b1294f46e4413cc71c3
buddhi desilva <buddhi.desilva@gmail.com>
2012-12-04 09:37:55 +0800

    test_dependancy_files_issue

--- lib/active_merchant/billing.rb
+++ lib/active_merchant/billing.rb
@@ -1,6 +1,6 @@
 require 'active_merchant/billing/avs_result'
 require 'active_merchant/billing/cvv_result'
-require 'active_merchant/billing/credit_card_methods'
+# require 'active_merchant/billing/credit_card_methods'
 require 'active_merchant/billing/credit_card_formatting'
 require 'active_merchant/billing/credit_card'
 require 'active_merchant/billing/base'
